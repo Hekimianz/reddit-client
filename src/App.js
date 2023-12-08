5ce95d54e6ec00ac1e05fc7e4b53f5afe7ba31a5
Hekimianz <aramhekdesign@gmail.com>
2023-12-08 14:03:11 -0600

    started designing components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import mockData from "./mockData";
+import Post from "./components/Post";
+import styles from "./css/App.module.css";
+
 function App() {
-  return <div></div>;
+  return (
+    <div className={styles.appCont}>
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Post.js
@@ -0,0 +1,6 @@
+import styles from "../css/Post.module.css";
+function Post() {
+  return <div className={styles.postCont}></div>;
+}
+
+export default Post;
