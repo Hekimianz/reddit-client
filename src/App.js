a2218a81cc0bf7c2c885c15eb0931a8781cf7288
Hekimianz <aramhekdesign@gmail.com>
2023-12-08 15:42:02 -0600

    fnished main home design

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import mockData from "./mockData";
+// import mockData from "./mockData";
 import Post from "./components/Post";
 import styles from "./css/App.module.css";
+import redditLogo from "./assets/reddit-logo.png";
 
 function App() {
   return (
     <div className={styles.appCont}>
+      <nav className={styles.nav}>
+        <img src={redditLogo} alt="reddit logo" />
+        <span className={styles.navTitle}>Reddit Minimal</span>
+      </nav>
       <Post />
       <Post />
       <Post />
       <Post />
+      <div className={styles.btnsCont}>
+        <button className={styles.btn}>Prev Page</button>
+        <button className={styles.btn}>Next Page</button>
+      </div>
     </div>
   );
 }
